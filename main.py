82ef8274f2427c5c35e49b53f49187f233c0fe43
deanroks1 <78750220+deanroks1@users.noreply.github.com>
2021-12-20 23:43:24 +0100

    Main.py updated
    Added basic stuff on start up

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
 import discord
 from discord.ext import commands
+
+
+bot = commands.Bot(
+  command_prefix = commands.when_mentioned_or('>'),
+  help_command=None,
+  intents=discord.Intents.all(),
+  bot=True,
+)
+
+# on start-up
+
+@bot.event
+async def on_ready():
+    print('[C]: Username: {0.user.name}#{0.user.discriminator}\n[C]: UserID: {0.user.id}\n[C]: Date Creation: {0.user.created_at}'.format(bot));
+    print(f'[C]: Currently in {int(len(bot.guilds))} servers!')
+
+    await bot.change_presence(activity=discord.Game(name="Negus bot v0.1"))
+
+
+
+
+
+
+
+
+
+
+
+bot.run("OTIyNjIwMTc1NjMzNzY4NDY4.YcEG_w.t8SuFPeeuLewD2YPLqC-Pptf67c")
